c25d06c32b8af007633786ef51df1187dd1356f2
Shawn Armstrong <smarmstr@ucsc.edu>
2023-06-22 20:22:39 -0700

    Add resume comment.

--- src/assets/details.js
+++ src/assets/details.js
@@ -3,7 +3,7 @@ module.exports = {
   name: 'FirstName LastName',
   university: 'University of California, Santa Cruz',
   major: 'Computer Science (B.S) - 2023',
-  resumeLink: 'HYPERLINK_TO_RESUME_USE_GOOGLE_DRIVE?',
+  resumeLink: 'HYPERLINK_TO_RESUME_USE_GOOGLE_DRIVE', // See https://tinyurl.com/create-resume-link
   cruzId: 'YOUR_CRUZ_ID',
   profileFileName: 'default_user.png', // Add image file to assets directory
   gitHubLink: 'YOUR_GITHUB_URL',
